2076d02e0afc8070bdde2eca58caa074648132f5
Faiq Ali <faiqueali017@gmail.com>
2021-07-30 20:21:36 +0500

    Implementing other endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -27,28 +27,63 @@ stores = [
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    # Iterate over stores
+    for store in stores:
+        # If the store name matches, return it
+        if store['name'] == name:
+            return jsonify(store)
+    # If none match, return an error message
+    return jsonify({'message': 'Store not found !!'})
+
 
 # GET /store
 @app.route('/store')
-def get_stores(name):
-    pass
+def get_stores():
+    return jsonify({'stores': stores})
+
 
 # POST /store/<string:name>/item {name:, price}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+
+    # Iterate over stores
+    for store in stores:
+        # If the store name matches, return it
+        if store['name'] == name:
+            # Create new item
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            # Return new item
+            return jsonify(new_item)
+    # Return message if store not found
+    return jsonify({'message': 'Store not found !!'})
+
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
-
+    # Iterate over stores
+    for store in stores:
+        # If the store name matches, return it
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    # Return message if store not found
+    return jsonify({'message': 'Store not found !!'})
 
 
 app.run(port=5000)
