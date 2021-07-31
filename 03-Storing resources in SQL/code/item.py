a71fcb80b8f05cc629fae27765abe42335f3a253
Faiq Ali <faiqueali017@gmail.com>
2021-07-31 22:19:54 +0500

    Retrieving all items

--- 03-Storing resources in SQL/code/item.py
+++ 03-Storing resources in SQL/code/item.py
@@ -113,4 +113,16 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
         return {'items': items}
