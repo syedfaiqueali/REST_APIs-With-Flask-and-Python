da0c6a3cd600178ccbbc95f5b0421815c7206a8d
Faiq Ali <faiqueali017@gmail.com>
2021-07-31 19:02:35 +0500

    Logging in and retrieving user from sqlite dB

--- 03-Storing resources in SQL/code/security.py
+++ 03-Storing resources in SQL/code/security.py
@@ -2,35 +2,10 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-
-# Table for registered users
-users = [
-    User(1, 'bob', 'asdf')
-]
-
-'''
-username_mapping = { 'bob': {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
-
-userid_mapping = { 1: {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
-'''
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
-
 # Function to authenticate the user
 def authenticate(username, password):
-    # Accessing dict using get(key, None(for not found key))
-    user = username_mapping.get(username, None)
+    # Looking user into dB
+    user = User.find_by_username(username)
 
     # Checking user not none and user's password matches
     if user and safe_str_cmp(user.password, password):
@@ -41,4 +16,4 @@ def authenticate(username, password):
 def identity(payload):
     # Extract user's if from the payload
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
