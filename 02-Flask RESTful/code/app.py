72cda67ec5e7370225eef4f291af0625dfb134c4
Faiq Ali <faiqueali017@gmail.com>
2021-07-31 09:49:24 +0500

    Improving code and error control

--- 02-Flask RESTful/code/app.py
+++ 02-Flask RESTful/code/app.py
@@ -10,13 +10,17 @@ items = []
 class Item(Resource):
     # Defining methods
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        # filter(filter_func, list_of_items)
+        # next would give first item found by the list but have to return none if no item found
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
 
     def post(self, name):
+        # If found an item so no need to add it just return message
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'An item with name '{}' already exists.'.format(name)}, 400
+
         # If the req doesn't have proper content type, this will trigger error
         # get_json(force,silent)
         # force=True ;dont need content-type header, nice but dangerous
