53d08800a57f500c26ce915636c0d6999fbb579b
Faiq Ali <faiqueali017@gmail.com>
2021-08-01 20:11:02 +0500

    Displaying the ItemList resources with SQLAlchemy

--- 04-Simplifying storage with Flask-SQLAlchemy/code/resources/item.py
+++ 04-Simplifying storage with Flask-SQLAlchemy/code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -75,16 +74,5 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-
-        return {'items': items}
+        # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))} # Using lambda func
+        return {'items': [item.json() for item in ItemModel.query.all()]} # Using list comprehension
