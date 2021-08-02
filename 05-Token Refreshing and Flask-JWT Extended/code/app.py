c0bf8489640ed455ccbc57ec4fbb0586f5c0f4b1
Faiq Ali <faiqueali017@gmail.com>
2021-08-02 22:24:27 +0500

    Customizing Flask-JWT-Extended callbacks and responses

--- 05-Token Refreshing and Flask-JWT Extended/code/app.py
+++ 05-Token Refreshing and Flask-JWT Extended/code/app.py
@@ -33,6 +33,44 @@ def add_claims_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
+# To what to tell user when their token is expired, i.e after 5mins
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+# When token sent us is not an actual JWT
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+# When user logged out so put that token in revoked tokens list
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
+
 
 # Adding resource and determine how its going to be access
 api.add_resource(Store, '/store/<string:name>')
