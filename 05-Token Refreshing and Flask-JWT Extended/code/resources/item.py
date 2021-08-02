df4dcc109c37b7ad7aaae68f6ece6514909197b6
Faiq Ali <faiqueali017@gmail.com>
2021-08-02 21:22:32 +0500

    Including jwt_identity in get items to check if user logged in

--- 05-Token Refreshing and Flask-JWT Extended/code/resources/item.py
+++ 05-Token Refreshing and Flask-JWT Extended/code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -85,6 +85,17 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))} # Using lambda func
-        return {'items': [item.json() for item in ItemModel.find_all()]} # Using list comprehension
+        user_id = get_jwt_identity() #give whatever we save in access token as identity
+        items = [item.json() for item in ItemModel.find_all()]
+
+        # Means user is still logged in
+        if user_id:
+            return {'items': items}, 200
+
+        # User not logged in
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data available if you log in.'
+        }, 200
