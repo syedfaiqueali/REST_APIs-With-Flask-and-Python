347b48495d26d6bf3e09ba50c94633857827995f
Faiq Ali <faiqueali017@gmail.com>
2021-08-04 18:23:08 +0500

    Adding an activated property for email confirmation

---
+++ 08- Email Confirmations/resources/item.py
@@ -0,0 +1,86 @@
+from flask_restful import Resource
+from flask import request
+from flask_jwt_extended import jwt_required, fresh_jwt_required
+from marshmallow import ValidationError
+from models.item import ItemModel
+from schemas.item import ItemSchema
+
+# Constants
+BLANK_ERROR = "{} cannot be blank."
+ERROR_INSERTING = "An error occured inserting the item."
+ERROR_UPDATING = "An error occured while updating the item."
+NAME_ALREDY_EXISTS = "An item with name {} already exists."
+ITEM_NOT_FOUND = "Item not found."
+ITEM_DELETED = "Item deleted."
+
+item_schema = ItemSchema()  # Single Item
+item_list_schema = ItemSchema(many=True)  # Many items
+
+
+class Item(Resource):
+    # Endpoints
+    @classmethod
+    def get(cls, name: str):
+        """
+        To select item from database with specific 'name'
+        """
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item_schema.dump(item), 200
+        return {"message": ITEM_NOT_FOUND}, 404
+
+    @classmethod
+    @fresh_jwt_required
+    def post(cls, name: str):
+        # If found an item so no need to add it just return message
+        if ItemModel.find_by_name(name):
+            return {
+                "message": NAME_ALREDY_EXISTS.format(name)
+            }, 400  # When req goes wrong
+
+        item_json = request.get_json()  # get user's sent json payload
+        item_json["name"] = name  # Populating json with items name
+
+        item = item_schema.load(item_json)
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message": ERROR_INSERTING}, 500  # Internal Server Error
+
+        return item_schema.dump(item), 201
+
+    @classmethod
+    @jwt_required
+    def delete(cls, name: str):
+        # Finding item
+        item = ItemModel.find_by_name(name)
+        # Checking if not null so delete
+        if item:
+            item.delete_from_db()
+            return {"message": ITEM_DELETED}, 200
+        return {"message": ITEM_NOT_FOUND}, 404
+
+    @classmethod
+    def put(cls, name: str):
+        # Getting items data
+        item_json = request.get_json()
+        item = ItemModel.find_by_name(name)
+
+        # updating
+        if item:
+            item.price = data["price"]
+        # Inserting
+        else:
+            item_json["name"] = name  # Populating json with item's name
+            item = item_schema.load(item_json)
+
+        # Commit changes
+        item.save_to_db()
+        return item_schema.dump(item), 200
+
+
+class ItemList(Resource):
+    @classmethod
+    def get(cls):
+        return {"items": item_list_schema.dump(ItemModel.find_all())}, 200
---
+++ 08- Email Confirmations/resources/user.py
@@ -0,0 +1,117 @@
+from flask_restful import Resource
+from flask import request
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt,
+)
+from models.user import UserModel
+from schemas.user import UserSchema
+from blacklist import BLACKLIST
+
+# Constants
+BLANK_ERROR = "{} cannot be blank."
+USER_ALREADY_EXISTS = "A user with that username already exists."
+CREATED_SUCCESSFULLY = "User created successfully."
+USER_NOT_FOUND = "User not found."
+USER_DELETED = "User deleted."
+INVALID_CREDENTIALS = "Invalid credentials!"
+USER_LOGGED_OUT = "User <id={}> successfully logged out."
+NOT_CONFIRMED_ERROR = (
+    "You have not confirmed registeration, please check your email <{}>."
+)
+
+# Creating Schema
+user_schema = UserSchema()
+
+
+class UserRegister(Resource):
+    # Endpoints
+    @classmethod
+    def post(cls):
+        # Creating a UserModel obj
+        user_json = request.get_json()
+        user = user_schema.load(user_json)
+
+        # Check if user already exists
+        if UserModel.find_by_username(user.username):
+            return {"message": USER_ALREADY_EXISTS}, 400
+
+        user.save_to_db()
+
+        return {"message": CREATED_SUCCESSFULLY}, 201
+
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+
+        # If user not found in db
+        if not user:
+            return {"message": USER_NOT_FOUND}, 404
+        # Return the found user
+        return user_schema.dump(user), 200
+
+    @classmethod
+    def delete(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+
+        # If user not found in db
+        if not user:
+            return {"message": USER_NOT_FOUND}, 404
+        # User founded, delete it
+        user.delete_from_db()
+        return {"message": USER_DELETED}, 200
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        # Passing dict from req to user_schema
+        user_json = request.get_json()
+        user_data = user_schema.load(user_json)  # Which we got from request
+
+        # find req data into the dB => returns obj
+        user = UserModel.find_by_username(user_data.username)
+
+        # check user and password => authenticate()
+        if user and safe_str_cmp(user.password, user_data.password):
+            #
+            if user.activated:
+                # create access and refresh token and return it => identity()
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(user.id)
+
+                return {
+                    "access_token": access_token,  # main jwt_token
+                    "refresh_token": refresh_token,
+                }, 200
+            return {"message": NOT_CONFIRMED_ERROR.format(user.username)}, 400
+
+        # User doesn't exists
+        return {"message": INVALID_CREDENTIALS}, 401
+
+
+class UserLogout(Resource):
+    @classmethod
+    @jwt_required
+    def post(cls):
+        # jti is JWT ID, a unique identifier for a JWT.
+        jti = get_raw_jwt()["jti"]
+        user_id = get_jwt_identity()
+        BLACKLIST.add(jti)
+        return {"message": USER_LOGGED_OUT.format(user_id)}, 200
+
+
+class TokenRefresh(Resource):
+    @classmethod
+    @jwt_refresh_token_required
+    def post(cls):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}, 200
