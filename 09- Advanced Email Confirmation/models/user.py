4c3b48d2fe13bd7988a4f1e5faa652996dfd8914
Faiq Ali <faiqueali017@gmail.com>
2021-08-07 21:49:18 +0500

    Changes in user model

--- 09- Advanced Email Confirmation/models/user.py
+++ 09- Advanced Email Confirmation/models/user.py
@@ -1,7 +1,9 @@
 from flask import request, url_for
 from requests import Response
+
 from db import db
 from libs.mailgun import Mailgun
+from models.confirmation import ConfirmationModel
 
 
 class UserModel(db.Model):
@@ -11,9 +13,20 @@ class UserModel(db.Model):
     # nullable=False ;will check whether req or not when loading data into ma
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False, unique=True)
-    activated = db.Column(db.Boolean, default=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    # lazy="dynamic" ;when new usermodel created, the ConfirmationModel not retrieved from the db when access confir. property then it access
+    # cascade="all, delete-orphan" ;when we delete a user, it going to confirmation and delete all his belongings
+    confirmation = db.relationship(
+        "ConfirmationModel", lazy="dynamic", cascade="all, delete-orphan"
+    )
+
+    @property
+    def most_recent_confirmation(self) -> "ConfirmationModel":
+        return self.confirmation.order_by(
+            db.desc(ConfirmationModel.expire_at)
+        ).first()  # expire_at(col) in desc order, then its desc_first() => newest
 
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
@@ -28,9 +41,12 @@ class UserModel(db.Model):
         return cls.query.filter_by(id=_id).first()
 
     def send_confirmation_email(self) -> Response:
-        # http://127.0.0.1:5000/ ;excluding last '/' + /user_confirm/1
-        link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
         subject = "Registration confirmation"
+
+        # http://127.0.0.1:5000/ ;excluding last '/' + /confirmation/confirmation_id
+        link = request.url_root[:-1] + url_for(
+            "confirmation", confirmation_id=self.most_recent_confirmation.id
+        )
         text = f"Please click the link to confirm your registeration: {link}"
         html = f'<html>Please click the link to confirm your registeration: <a href="{link}">{link}</a></html>'
 
