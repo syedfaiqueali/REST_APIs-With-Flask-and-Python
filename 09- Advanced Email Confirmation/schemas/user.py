fd495951b277a18d44d9c1e50f68d8ee0d24831a
Faiq Ali <faiqueali017@gmail.com>
2021-08-07 22:41:31 +0500

    Adding the last confirmation to the user schema

--- 09- Advanced Email Confirmation/schemas/user.py
+++ 09- Advanced Email Confirmation/schemas/user.py
@@ -1,3 +1,5 @@
+from marshmallow import pre_dump
+
 from ma import ma
 from models.user import UserModel
 
@@ -7,4 +9,13 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel  # Go into UserModel and look into column def & create marshmallow fields
         load_only = ("password",)  # Will not return(dump) password by doing this
-        dump_only = ("id", "activated")  # Will not be looked at when creating the model
+        dump_only = (
+            "id",
+            "confirmation",
+        )  # Will not be looked at when creating the model
+
+    @pre_dump
+    def _pre_dump(self, user: UserModel):
+        """Whenever we resent the confirmation, the schema will not include old expired confirmations."""
+        user.confirmation = [user.most_recent_confirmation]
+        return user
