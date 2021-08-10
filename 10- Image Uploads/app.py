b4c0d509f9c86cff5dbac58ef1ff623bf47fca4a
Faiq Ali <faiqueali017@gmail.com>
2021-08-10 23:48:50 +0500

    Retrieving and deleting images

--- 10- Image Uploads/app.py
+++ 10- Image Uploads/app.py
@@ -15,7 +15,7 @@ from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogo
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
-from resources.image import ImageUpload
+from resources.image import ImageUpload, Image
 from libs.image_helper import IMAGE_SET
 
 app = Flask(__name__)
@@ -70,6 +70,7 @@ api.add_resource(ItemList, "/items")
 api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
 api.add_resource(ImageUpload, "/upload/image")
+api.add_resource(Image, "/image/<string:filename>")
 
 
 if __name__ == "__main__":
--- 10- Image Uploads/resources/image.py
+++ 10- Image Uploads/resources/image.py
@@ -1,6 +1,9 @@
+import traceback
+import os
+
 from flask_restful import Resource
 from flask_uploads import UploadNotAllowed
-from flask import request
+from flask import send_file, request  #send_file ; allows to send file in binary instead of json
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from libs import image_helper
@@ -30,3 +33,43 @@ class ImageUpload(Resource):
             return {
                 "message": gettext("image_illegal_extension").format(extension)
             }, 400
+
+
+class Image(Resource):
+    @jwt_required
+    def get(self, filename: str):
+        """
+        Returns the requested image if it exists. Looks up inside the logged in user's folder.
+        """
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+
+        #Check if filed saved or not
+        if not image_helper.is_filename_safe(filename):
+            return {"message": gettext("image_illegal_file_name").format(filename)}, 400
+
+        try:
+            return send_file(image_helper.get_path(filename, folder=folder))
+        except FileNotFoundError:
+            return {"message": gettext("image_not_found").format(filename)}, 404
+
+    @jwt_required
+    def delete(self, filename: str):
+        """
+        Deletes the requested image if it exists. Looks up inside the logged in user's folder.
+        """
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+
+        #Check if filed saved or not
+        if not image_helper.is_filename_safe(filename):
+            return {"message": gettext("image_illegal_file_name").format(filename)}, 400
+
+        try:
+            os.remove(image_helper.get_path(filename, folder=folder)) #remove folder(delete)
+            return {"message": gettext("image_deleted").format(filename)}, 200
+        except FileNotFoundError:
+            return {"message": gettext("image_not_found").format(filename)}, 404
+        except:
+            traceback.print_exc()
+            return {"message": gettext("image_delete_failed")}, 500
