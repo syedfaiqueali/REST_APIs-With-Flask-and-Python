0380ce773fc118524d118d46170cc49d12c00e34
Faiq Ali <faiqueali017@gmail.com>
2021-08-14 23:05:49 +0500

    Many to Many relationships with SQLAlchemy

--- 13- Payments with Stripe/models/order.py
+++ 13- Payments with Stripe/models/order.py
@@ -3,13 +3,25 @@ import os
 from db import db
 from typing import List
 
+# Defining table without Model, Table Id col would be default
+# [id, item_id, order_id] => Columns
+# [1, 3, 1]
+# [2, 6, 1]
+items_to_orders = db.Table(
+    "items_to_orders",  # Table name
+    db.Column("item_id", db.Integer, db.ForeignKey("items.id")),
+    db.Column("order_id", db.Integer, db.ForeignKey("orders.id"))
+)
+
 class OrderModel(db.Model):
     __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(20), nullable=False)
 
-    items = db.relationship("ItemModel", lazy="dynamic")
+    # Give a secondary table(items_to_orders) to go and find the items associated with the order
+    # Many to many relationship
+    items = db.relationship("ItemModel", secondary=items_to_orders, lazy="dynamic")
 
     @classmethod
     def find_all(cls) -> List["OrderModel"]:
