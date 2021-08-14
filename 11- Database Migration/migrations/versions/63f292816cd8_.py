f0467fd0d7e09d3a9c93380057c0833b34868dea
Faiq Ali <faiqueali017@gmail.com>
2021-08-14 10:38:16 +0500

    Defining SQLAlchemy naming convention

--- 11- Database Migration/migrations/versions/63f292816cd8_.py
+++ 11- Database Migration/migrations/versions/63f292816cd8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: dd752572e276
+Revision ID: 63f292816cd8
 Revises: 
-Create Date: 2021-08-14 00:37:04.391489
+Create Date: 2021-08-14 10:26:45.130876
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'dd752572e276'
+revision = '63f292816cd8'
 down_revision = None
 branch_labels = None
 depends_on = None
--- 11- Database Migration/models/user.py
+++ 11- Database Migration/models/user.py
@@ -5,7 +5,7 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
     @classmethod
